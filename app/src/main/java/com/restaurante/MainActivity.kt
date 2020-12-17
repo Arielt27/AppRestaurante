9026aed476a236167385c319042e35bdb37ea5e7
Jonathan Laux <jonathan.laux@hotmail.com>
2020-12-16 18:08:19 -0600

    Conexion a Firebase

--- app/src/main/java/com/restaurante/MainActivity.kt
+++ app/src/main/java/com/restaurante/MainActivity.kt
@@ -1,13 +1,24 @@
 package com.restaurante
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+
 
 class MainActivity : AppCompatActivity()
 {
+    private var mAuth: FirebaseAuth? = null
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val currentUser = mAuth?.currentUser
+        Log.d("RateRestaurant",currentUser.toString())
     }
 }
