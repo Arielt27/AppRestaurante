99f41878d333c38e6a036821e48e6c99b79deb0a
Jonathan Laux <jonathan.laux@hotmail.com>
2020-12-16 20:40:17 -0600

    Login

--- app/src/main/java/com/restaurante/MainActivity2.kt
+++ app/src/main/java/com/restaurante/MainActivity2.kt
@@ -1,13 +1,35 @@
 package com.restaurante
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
 
-class MainActivity2 : AppCompatActivity()
-{
-    override fun onCreate(savedInstanceState: Bundle?)
-    {
+class MainActivity2 : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var db: FirebaseDatabase
+    private lateinit var autoresRef: DatabaseReference
+
+    val TAG = "FBDemo"
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        auth= FirebaseAuth.getInstance()
+        /*btnCerrarSesion.setOnClickListener {
+            auth.signOut()
+            finish()
+        }*/
+        db = FirebaseDatabase.getInstance()
     }
+
+
+
+
+
+
 }
